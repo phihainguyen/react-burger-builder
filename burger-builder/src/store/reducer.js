1e0927f4c19d3a72757f7ef04d865a212d93263f
phihainguyen <phihai93@gmail.com>
2020-03-21 17:54:56 -0700

    update

---
+++ burger-builder/src/store/reducer.js
@@ -0,0 +1,13 @@
+import * as actionType from './action';
+
+const initialState = {
+	ingredients: null,
+	totalPrice: 4
+};
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionType.ADD_INGREDIENT:
+	}
+};
+
+export default reducer;
