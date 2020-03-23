436debbe64760bc85f259bbc8ecfa26ebf4a6c24
phihainguyen <phihai93@gmail.com>
2020-03-22 21:07:24 -0700

    combined reducer

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -6,14 +6,21 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reducer from './store/reducer/burgerBuilder';
-
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import burgerBuilderReducer from './store/reducer/burgerBuilder';
+import orderReducer from './store/reducer/order';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const rootReducer = combineReducers({
+	burgerBuilder: burgerBuilderReducer,
+	order: orderReducer
+});
+
+//store
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
 const app = (
 	<Provider store={store}>
 		<BrowserRouter>
